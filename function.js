c19b488339572efb5b54daa8b03d99d2e986ab62
yasmin <yasmin.alhajjaj@gmail.com>
2019-07-13 18:46:18 +0300

    fun1

--- function.js
+++ function.js
@@ -1,136 +1,101 @@
 /*
 1
-Create a function called sum 
-that takes a single parameter n, 
-and return the sum of all integers up to n starting from 0
-​
-Ex: sum(2); => 2 + 1 + 0 => 3 
-Ex: sum(5); => 5 + 4 + 3 + 2 + 1 + 0 => 15
-​
+Write a function named tellFortune that:
+takes 4 arguments: number of children, 
+partner's name, geographic location, job title.
+outputs your fortune to the screen like so: 
+"You will be a X in Y, and married to Z with N kids."
 */
-function sum(n) {
-  if (n==0){
-    return 0;
-  }
-  return n+sum(n-1);
-  // your code here 
-}
-console.log(sum(4));
-​
+
+
 /*
 2
-Create a function called factorial 
-that takes a single parameter n, 
-and return the product of all integers up to n starting from 1
-​
-Ex: factorial(2); => 2 * 1 => 6 
-Ex: factorial(4); => 4 * 3 * 2 * 1 => 24 
-​
+Write a function named calculateDogAge that:
+takes 1 argument: your puppy's age.
+calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
 */
-function factorial(n) {
-​
-  if(n===1){
-​
-    return 1;
-  
-  }
-return n =n* factorial(n-1);
- 
-}
-console.log(factorial(4))
-​
+
+
 /*
 3
-Create a function called repeatStr
-that takes 2 parameters string and number,
-and return the string number time with space
-​
-Ex: repeatStr("to",2); => "to to"
-Ex: repeatStr("to",4); => "to to to to"
+Write a function named calculateSupply that:
+takes 2 arguments: age, amount per day.
+calculates the amount consumed for rest of the life (based on a constant max age).
+outputs the result to the screen like so: "You will need NN to last you until the ripe old age of X"
 */
-function repeatStr(x,y){
-​
-if (y===0){
-  return"";
-}
-return x+ repeatStr(x,y-1)
- 
-}
-​
-console.log(repeatStr("mohd ",5))
-​
+
+
 /*
 4
-Create a function called fibon
-that takes 1 parameter a number,
-and return the fibonacci for this number
-​
-Ex: fibon(7); => 21
-Ex: fibon(2); => 2
-Ex: fibon(1); => 1
-Ex: fibon(0); => 1
-​
-fib(n): 1 1 2 3 5 8 13 21
-        | | | | | |  |  |
-n:      0 1 2 3 4 5  6  7
+Write a function called greet that:
+takes 1 argument: name.
+and it will return hello + name
 */
-function ficccccbon(x){
-​
-  if(x===1||x===0){
-    return 1;
-  }
-return ficccccbon(x-1)+ficccccbon(x-2);
-​
-}
-console.log(ficccccbon(5));
-​
-​
-​
+
+
 /*
 5
-Create a function called sum2
-that take two parameter
-and will return the sumation from the first number to the second number
-Ex: sum2(4, 5); => 4 + 5 => 9
-Ex: sum2(3, 6); => 3 + 4 + 5 + 6 => 18
+what is the error:
 */
-function sum2(x,y){
-​
-if(y=(x+1))
-{
-  return x+y;
+function double(cat) {
+  return 2 * x;
 }
+
+function double(7) {
+  return 2 * 7;
 }
-console.log(sum2(6,7))
-​
+
+function double('7') {
+  return 2 * 'x';
+}
+
+
 /*
 6
-Create a function called repeatStr2
-that takes 2 string parameters,
-and return the first string number time
-(of the lenght of the secend string) with space betweeen it
-Important: the stop condition should be [somthing=== 0]
-Ex: repeatStr2("ro","cc"); => "ro ro"
-Ex: repeatStr2("ro","fff"); => "ro ro ro"
+fix these functions
 */
-​
-​
+func double1(x {
+  return 2 * x ;
+} 
+
+functiondouble2 x)
+return 2 * x; 
+}
+
+function (x) double3 {
+  return 2 * x;
+
+
 /*
 7
-Create a function called multiOf
-that takes 3 parameters,
-and return the first number mutiple by
-the second one ^ the third one
-Ex: multiOf(4,3,10); => 4000
-Ex: multiOf(6,2,4); => 96
+Write a function called cube that:
+accept 1 parameter and calculate the cube of it
 */
-​
-​
+
+
+
+
 /*
-8
-Create a function called muti2
-that take two parameter
-and will return the multiplication from the first number to the second number
-Ex: muti2(4, 5); => 4 * 5 => 20
-Ex: muti2(3, 6); => 3 * 4 * 5 * 6 => 360
+
+
+
+*/
+
+/*
+
+
+
+*/
+
+/*
+
+
+
+*/
+
+/*
+
+
+
 */
